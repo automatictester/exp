1ffa3e7c55a0a263204223e7adc19f189b7e7d4c
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-04-05 10:26:49 +0100

    Add DNS resolution

--- jenkins-on-aws/jenkins-start.sh
+++ jenkins-on-aws/jenkins-start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script requires 'vars.sh' declaring JENKINS_MASTER_DOMAIN_NAME and HOSTED_ZONE_ID variables
+
 PUBLIC_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo "Your public IP: ${PUBLIC_IP}"
 
@@ -17,12 +19,18 @@ aws ec2 authorize-security-group-ingress --group-id ${JENKINS_MASTER_SECURITY_GR
 OLD_HTTPS_CIDR=$(aws ec2 describe-security-groups --filters "Name=description,Values=SSH and Jenkins HTTPS from my public IP only" --query 'SecurityGroups[*].IpPermissions[?FromPort==`8081`].IpRanges[*].CidrIp' --output text)
 aws ec2 revoke-security-group-ingress --group-id ${JENKINS_MASTER_SECURITY_GROUP_ID} --protocol tcp --port 8081 --cidr ${OLD_HTTPS_CIDR}
 aws ec2 authorize-security-group-ingress --group-id ${JENKINS_MASTER_SECURITY_GROUP_ID} --protocol tcp --port 8081 --cidr "${PUBLIC_IP}/32"
-
 echo 'done!'
 
 echo -n 'Starting Jenkins master... '
 aws ec2 start-instances --instance-ids ${JENKINS_MASTER_INSTANCE_ID} --output text >> /dev/null
 echo 'done!'
 
-URL=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Jenkins Master' "Name=instance-id,Values=${JENKINS_MASTER_INSTANCE_ID}" --output text --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddresses[*].Association.PublicDnsName')
-echo -e "URL:\nhttps://${URL}:8081"
+echo -n 'Updating DNS record... '
+source vars.sh
+JENKINS_MASTER_PUBLIC_IP=$(aws ec2 describe-instances --filters 'Name=tag:Name,Values=Jenkins Master' --output text --query 'Reservations[*].Instances[*].PublicIpAddress')
+aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch "{ \"Changes\": [ { \"Action\": \"UPSERT\", \"ResourceRecordSet\": { \"Name\": \"${JENKINS_MASTER_DOMAIN_NAME}\", \"Type\": \"A\", \"TTL\": 60, \"ResourceRecords\": [ { \"Value\": \"${JENKINS_MASTER_PUBLIC_IP}\" } ] } } ] }" >> /dev/null
+echo 'done!'
+
+echo -n 'Flushing local DNS cache... '
+sudo killall -HUP mDNSResponder
+echo 'done!'
