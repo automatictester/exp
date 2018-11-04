f3dd735ef6785cb45680a627e1c6082a1d8b4992
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-04 17:23:54 +0000

    Add BinaryTree -- WIP
    TODO:
    - delete operation for top node with two leafs and right-left leaf
    - delete operation for non-top node with two leafs and right-left-right leaf
    - delete operation for top node with two leafs and right-left-right leaf

---
+++ java/data-structures/src/main/java/uk/co/automatictester/binarytree/BinaryTree.java
@@ -0,0 +1,155 @@
+package uk.co.automatictester.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("WeakerAccess")
+public class BinaryTree<T extends Comparable<T>> {
+
+    BinaryTree<T> left;
+    T value;
+    BinaryTree<T> right;
+
+    public BinaryTree() {
+    }
+
+    public BinaryTree(T element) {
+        this.value = element;
+    }
+
+    public void insert(T element) {
+        if (value == null) {
+            value = element;
+        } else if (element.compareTo(value) < 0 && left == null) {
+            left = new BinaryTree<>(element);
+        } else if (element.compareTo(value) < 0 && left != null) {
+            left.insert(element);
+        } else if (element.compareTo(value) > 0 && right == null) {
+            right = new BinaryTree<>(element);
+        } else {
+            right.insert(element);
+        }
+    }
+
+    public boolean contains(T element) {
+        if (value == null) {
+            return false;
+        } else if (value.equals(element)) {
+            return true;
+        } else if (element.compareTo(value) < 0 && left == null) {
+            return false;
+        } else if (element.compareTo(value) < 0 && left != null) {
+            return left.contains(element);
+        } else if (element.compareTo(value) > 0 && right == null) {
+            return false;
+        } else {
+            return right.contains(element);
+        }
+    }
+
+    public List<T> toList() {
+        List<T> list = new ArrayList<>();
+        if (left != null) {
+            list.addAll(left.toList());
+        }
+        if (value != null) {
+            list.add(value);
+        }
+        if (right != null) {
+            list.addAll(right.toList());
+        }
+        return list;
+    }
+
+    public void delete(T element) {
+        delete(null, element);
+    }
+
+    public void delete(BinaryTree<T> parent, T element) {
+        if (value == null) {
+            return;
+        } else if (value == element && left == null && right == null) {
+            value = null;
+        } else if (value == element && left == null) {
+            if (parent != null && parent.left.equals(this)) {
+                parent.left = right;
+                value = null;
+                right = null;
+            } else if (parent != null && parent.right.equals(this)) {
+                parent.right = right;
+                value = null;
+                right = null;
+            } else if (parent == null) {
+                value = right.value;
+                right = right.left;
+            }
+        } else if (value == element && right == null) {
+            if (parent != null && parent.left.equals(this)) {
+                parent.left = left;
+                value = null;
+                left = null;
+            } else if (parent != null && parent.right.equals(this)) {
+                parent.right = left;
+                value = null;
+                left = null;
+            } else if (parent == null) {
+                value = left.value;
+                left = left.left;
+            }
+        } else if (value == element && left != null && right != null && right.left == null) {
+            right.left = left;
+            if (parent != null && parent.left != null && parent.left.equals(this)) {
+                parent.left = right;
+                value = null;
+                left = null;
+                right = null;
+            } else if (parent != null && parent.right != null && parent.right.equals(this)) {
+                parent.right = right;
+                value = null;
+                left = null;
+                right = null;
+            } else if (parent == null) {
+                value = right.value;
+                right = null;
+            }
+        } else if (value == element && left != null && right != null && right.left != null) {
+            BinaryTree<T> rightLeftMostChild = right.leftMostChild();
+            BinaryTree<T> leftMostChildParent = right.leftMostChildParent(this);
+            rightLeftMostChild.left = left;
+            rightLeftMostChild.right = right;
+            leftMostChildParent.left = null;
+            if (parent.left != null && parent.left.equals(this)) {
+                parent.left = rightLeftMostChild;
+            } else {
+                parent.right = rightLeftMostChild;
+            }
+            value = null;
+            left = null;
+            right = null;
+        } else if (element.compareTo(value) < 0 && left == null) {
+            return;
+        } else if (element.compareTo(value) < 0 && left != null) {
+            left.delete(this, element);
+        } else if (element.compareTo(value) > 0 && right == null) {
+            return;
+        } else if (element.compareTo(value) > 0 && right != null) {
+            right.delete(this, element);
+        }
+    }
+
+    private BinaryTree<T> leftMostChild() {
+        if (left == null) {
+            return this;
+        } else {
+            return left.leftMostChild();
+        }
+    }
+
+    private BinaryTree<T> leftMostChildParent(BinaryTree<T> parent) {
+        if (left == null) {
+            return parent;
+        } else {
+            return left.leftMostChildParent(this);
+        }
+    }
+}
---
+++ java/data-structures/src/test/java/uk/co/automatictester/binarytree/BinaryTreeTest.java
@@ -0,0 +1,327 @@
+package uk.co.automatictester.binarytree;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.assertTrue;
+
+@SuppressWarnings("SimplifiedTestNGAssertion")
+public class BinaryTreeTest {
+
+    @Test
+    public void testDefaultConstructor() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        assertTrue(tree.left == null);
+        assertTrue(tree.value == null);
+        assertTrue(tree.right == null);
+    }
+
+    @Test
+    public void testConstructor() {
+        BinaryTree<Integer> tree = new BinaryTree<>(1);
+        assertTrue(tree.left == null);
+        assertTrue(tree.value == 1);
+        assertTrue(tree.right == null);
+    }
+
+    @Test
+    public void testInsertEmpty() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(1);
+        assertTrue(tree.value == 1);
+    }
+
+    @Test
+    public void testInsertEmptyLeft() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.insert(5);
+        assertTrue(tree.left.value == 5);
+        assertTrue(tree.value == 10);
+    }
+
+    @Test
+    public void testInsertEmptyRight() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.insert(20);
+        assertTrue(tree.value == 10);
+        assertTrue(tree.right.value == 20);
+    }
+
+    @Test
+    public void testInsertLeft() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.insert(5);
+        tree.insert(2);
+        assertTrue(tree.left.left.value == 2);
+        assertTrue(tree.left.value == 5);
+        assertTrue(tree.value == 10);
+    }
+
+    @Test
+    public void testInsertRight() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.insert(20);
+        tree.insert(25);
+        assertTrue(tree.value == 10);
+        assertTrue(tree.right.value == 20);
+        assertTrue(tree.right.right.value == 25);
+    }
+
+    @Test
+    public void testContains() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        assertTrue(!tree.contains(10));
+        tree.insert(10);
+        assertTrue(tree.contains(10));
+        assertTrue(!tree.contains(5));
+        assertTrue(!tree.contains(20));
+        tree.insert(5);
+        assertTrue(tree.contains(5));
+        tree.insert(2);
+        assertTrue(tree.contains(2));
+        tree.insert(20);
+        assertTrue(tree.contains(20));
+        tree.insert(25);
+        assertTrue(tree.contains(25));
+    }
+
+    @Test
+    public void testToListBalanced() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(25);
+        List<Integer> list = Arrays.asList(2, 5, 7, 10, 15, 20, 25);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testToList() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(2);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(10);
+        tree.insert(15);
+        tree.insert(20);
+        tree.insert(25);
+        List<Integer> list = Arrays.asList(2, 5, 7, 10, 15, 20, 25);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteFromEmpty() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.delete(10);
+    }
+
+    @Test
+    public void testDeleteOnlyElement() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.delete(10);
+        List<Integer> list = Collections.emptyList();
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLowerThanOnlyElement() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.delete(7);
+        List<Integer> list = Collections.singletonList(10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteGreaterThanOnlyElement() {
+        BinaryTree<Integer> tree = new BinaryTree<>(10);
+        tree.delete(20);
+        List<Integer> list = Collections.singletonList(10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLeftMostLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(25);
+        tree.delete(2);
+        List<Integer> list = Arrays.asList(5, 7, 10, 15, 20, 25);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteRightMostLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(25);
+        tree.delete(25);
+        List<Integer> list = Arrays.asList(2, 5, 7, 10, 15, 20);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLeftNodeWithLeftLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.delete(2);
+        List<Integer> list = Arrays.asList(1, 5, 6, 7, 8, 10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLeftNodeWithRightLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(3);
+        tree.insert(6);
+        tree.insert(8);
+        tree.delete(2);
+        List<Integer> list = Arrays.asList(3, 5, 6, 7, 8, 10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteTopNodeWithLeftLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.delete(10);
+        List<Integer> list = Collections.singletonList(5);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteTopNodeWithRightLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(20);
+        tree.delete(10);
+        List<Integer> list = Collections.singletonList(20);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteRightNodeWithLeftLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(6);
+        tree.delete(7);
+        List<Integer> list = Arrays.asList(1, 2, 3, 5, 6, 10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteRightNodeWithRightLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(2);
+        tree.insert(7);
+        tree.insert(1);
+        tree.insert(3);
+        tree.insert(8);
+        tree.delete(7);
+        List<Integer> list = Arrays.asList(1, 2, 3, 5, 8, 10);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLeftNodeWithTwoLeafs() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(20);
+        tree.insert(2);
+        tree.insert(15);
+        tree.insert(1);
+        tree.insert(3);
+        tree.delete(2);
+        List<Integer> list = Arrays.asList(1, 3, 5, 10, 15, 20);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteRightNodeWithTwoLeafs() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(20);
+        tree.insert(8);
+        tree.insert(15);
+        tree.insert(6);
+        tree.insert(9);
+        tree.delete(8);
+        List<Integer> list = Arrays.asList(5, 6, 9, 10, 15, 20);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteTopNodeWithTwoLeafs() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(20);
+        tree.delete(10);
+        List<Integer> list = Arrays.asList(5, 20);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteRightNodeWithTwoLeafsAndRightLeftLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(25);
+        tree.insert(22);
+        tree.insert(30);
+        tree.delete(20);
+        List<Integer> list = Arrays.asList(10, 15, 22, 25, 30);
+        assertTrue(tree.toList().equals(list));
+    }
+
+    @Test
+    public void testDeleteLeftNodeWithTwoLeafsAndRightLeftLeaf() {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+        tree.insert(60);
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(25);
+        tree.insert(22);
+        tree.insert(30);
+        tree.delete(20);
+        List<Integer> list = Arrays.asList(15, 22, 25, 30, 60);
+        assertTrue(tree.toList().equals(list));
+    }
+}
