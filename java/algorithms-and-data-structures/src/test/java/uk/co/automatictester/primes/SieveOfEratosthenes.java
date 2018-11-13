43949ba4bd7271d7821ccc6e17ca10ea859432f1
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-13 21:32:41 +0000

    Make Java 8 sieve non-recursive

--- java/algorithms-and-data-structures/src/test/java/uk/co/automatictester/primes/SieveOfEratosthenes.java
+++ java/algorithms-and-data-structures/src/test/java/uk/co/automatictester/primes/SieveOfEratosthenes.java
@@ -2,8 +2,9 @@ package uk.co.automatictester.primes;
 
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,16 +13,16 @@ import static java.lang.Math.sqrt;
 public class SieveOfEratosthenes {
 
     private static final int START = 2;
-    private static final int LIMIT = 1_000;
+    private static final int LIMIT = 50_000;
 
     @Test(invocationCount = 4)
-    public void linearSieve() {
-        List<Integer> input = IntStream.rangeClosed(START, LIMIT).boxed().collect(Collectors.toList());
-        List<Integer> toRemove = new ArrayList<>();
-        List<Integer> target = new ArrayList<>(input);
+    public void preJava8() {
+        List<Integer> potentialPrimes = IntStream.rangeClosed(START, LIMIT).boxed().collect(Collectors.toList());
+        Set<Integer> toRemove = new HashSet<>();
+        Set<Integer> target = new HashSet<>(potentialPrimes);
 
-        for (int i = 0; i < input.size() && input.get(i) < sqrt(LIMIT); i++) {
-            int elementValue = input.get(i);
+        for (int i = 0; i < potentialPrimes.size() && potentialPrimes.get(i) < sqrt(LIMIT); i++) {
+            int elementValue = potentialPrimes.get(i);
 
             for (int j = elementValue * 2; j <= LIMIT; j += elementValue) {
                 if (j % elementValue == 0) {
@@ -36,26 +37,20 @@ public class SieveOfEratosthenes {
     }
 
     @Test(invocationCount = 4)
-    public void recursiveSieve() {
-        List<Integer> input = IntStream.rangeClosed(START, LIMIT).boxed().collect(Collectors.toList());
-        sieveNextElement(input, new ArrayList<>());
-    }
+    public void java8() {
+        List<Integer> potentialPrimes = IntStream.rangeClosed(START, LIMIT).boxed().collect(Collectors.toList());
+        Set<Integer> primes = new HashSet<>();
 
-    private void sieveNextElement(List<Integer> potentialPrimes, List<Integer> primes) {
-        if (!potentialPrimes.isEmpty()) {
-
+        while (!potentialPrimes.isEmpty()) {
             int head = potentialPrimes.get(0);
             primes.add(head);
-
-            potentialPrimes.removeIf(integer -> integer % head == 0);
-            sieveNextElement(potentialPrimes, primes);
-
-        } else {
-            print(primes);
+            potentialPrimes.removeIf(x -> x % head == 0);
         }
+
+        print(primes);
     }
 
-    private void print(List<Integer> primes) {
+    private void print(Set<Integer> primes) {
 //        System.out.println(primes.toString());
         System.out.println(primes.size());
     }
