d465d368fe3672ff7f21f3813cfd19cc7b855a28
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-14 10:14:54 +0000

    Add Array Intersection

---
+++ java/algorithms-and-data-structures/src/main/java/uk/co/automatictester/sortedarraysintersection/ArrayIntersection.java
@@ -0,0 +1,34 @@
+package uk.co.automatictester.sortedarraysintersection;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayIntersection {
+
+    private ArrayIntersection() {
+    }
+
+    public static List<Integer> find(int[] a, int[] b) {
+        List<Integer> intersection = new LinkedList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int aIndex = 0;
+        int bIndex = 0;
+
+        while (aIndex < a.length && bIndex < b.length) {
+            if (a[aIndex] == b[bIndex]) {
+                intersection.add(a[aIndex]);
+                aIndex++;
+                bIndex++;
+            } else if (a[aIndex] < b[bIndex]) {
+                aIndex++;
+            } else if (a[aIndex] > b[bIndex]) {
+                bIndex++;
+            }
+        }
+
+        return intersection;
+    }
+}
---
+++ java/algorithms-and-data-structures/src/test/java/uk/co/automatictester/sortedarraysintersection/SortedArraysIntersectionTest.java
@@ -0,0 +1,52 @@
+package uk.co.automatictester.sortedarraysintersection;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class SortedArraysIntersectionTest {
+
+    @Test
+    public void testSame() {
+        int[] a = new int[]{1,2,3,4,5};
+        int[] b = new int[]{5,1,2,3,4};
+        List<Integer> intersection = ArrayIntersection.find(a, b);
+        assertEquals(intersection, Arrays.asList(1,2,3,4,5));
+    }
+
+    @Test
+    public void testBothEmpty() {
+        int[] a = new int[]{};
+        int[] b = new int[]{};
+        List<Integer> intersection = ArrayIntersection.find(a, b);
+        assertEquals(intersection, Collections.emptyList());
+    }
+
+    @Test
+    public void testOneEmpty() {
+        int[] a = new int[]{1,2,3,4,5};
+        int[] b = new int[]{};
+        List<Integer> intersection = ArrayIntersection.find(a, b);
+        assertEquals(intersection, Collections.emptyList());
+    }
+
+    @Test
+    public void testDiff() {
+        int[] a = new int[]{1,2,5,4,3};
+        int[] b = new int[]{6,7,10,9,8};
+        List<Integer> intersection = ArrayIntersection.find(a, b);
+        assertEquals(intersection, Collections.emptyList());
+    }
+
+    @Test
+    public void testOverlap() {
+        int[] a = new int[]{1,5,3,4,2};
+        int[] b = new int[]{5,10,1};
+        List<Integer> intersection = ArrayIntersection.find(a, b);
+        assertEquals(intersection, Arrays.asList(1,5));
+    }
+}
