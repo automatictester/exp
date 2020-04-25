08c42b492391dc855c6f64f6060acf7fca729152
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-04-25 11:07:11 +0200

    Interruptible Callable example

--- java/concurrency/src/test/java/uk/co/automatictester/concurrency/basics/InterruptibleThread.java
+++ java/concurrency/src/test/java/uk/co/automatictester/concurrency/basics/InterruptibleThread.java
@@ -3,6 +3,13 @@ package uk.co.automatictester.concurrency.basics;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @Slf4j
 public class InterruptibleThread {
 
@@ -10,13 +17,10 @@ public class InterruptibleThread {
     public void interruptLongRunningThreadRunnable() throws Exception {
 
         Runnable r = () -> {
-            for (int i = 0; i < 1_000_000; i++) {
-                if (Thread.currentThread().isInterrupted()) {
-                    log.info("Thread interrupted at iteration: {}", i);
-                    return;
-                }
+            while (!Thread.currentThread().isInterrupted()) {
             }
-            log.info("All done!"); // never executed due to t.interrupt()
+            // Runnable cannot throw exceptions
+            log.info("Thread interrupted");
         };
 
         Thread t = new Thread(r);
@@ -26,5 +30,29 @@ public class InterruptibleThread {
         t.interrupt();
 
         t.join();
+    }
+
+    @Test
+    public void interruptLongRunningThreadCallable() throws Exception {
+
+        Callable<Integer> c = () -> {
+            while (!Thread.currentThread().isInterrupted()) {
+            }
+            throw new InterruptedException();
+        };
+
+        FutureTask<Integer> ft = new FutureTask<>(c);
+        Thread t = new Thread(ft);
+        t.start();
+
+        Thread.sleep(1); // let the thread start
+        t.interrupt();
+
+        try {
+            ft.get(); // t.join() won't do the job with FutureTask
+        } catch (ExecutionException e) {
+            log.info("{}", e.getCause().toString());
+            assertThat(e.getCause().getClass(), equalTo(InterruptedException.class));
+        }
     }
 }
