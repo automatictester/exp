63b2996b14021820f5665417f219317a582521d1
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-04-26 19:00:20 +0200

    Extend RejectedExecutionHandler to demo both cases per policy: submit after shutdown & queue full

--- java/concurrency/src/test/java/uk/co/automatictester/concurrency/classes/executor/RejectedExecutionHandlerClass.java
+++ java/concurrency/src/test/java/uk/co/automatictester/concurrency/classes/executor/RejectedExecutionHandlerClass.java
@@ -6,55 +6,98 @@ import org.testng.annotations.Test;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
 @Slf4j
 public class RejectedExecutionHandlerClass {
 
     @Test(expectedExceptions = RejectedExecutionException.class)
     public void saturationPolicyDefaultAbort() throws InterruptedException {
-        Runnable r = () -> log.info("running");
-        ExecutorService service = Executors.newSingleThreadExecutor();
-        service.shutdown();
-        service.submit(r); // throws RejectedExecutionException
-        service.awaitTermination(10, TimeUnit.SECONDS);
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.AbortPolicy();
+        submitAfterShutdown(rejectedExecutionHandler); // throws RejectedExecutionException
     }
 
     @Test
     public void saturationPolicyDiscard() throws InterruptedException {
-        Runnable r = () -> log.info("running");
-
-        ExecutorService service = Executors.newCachedThreadPool();
         RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.DiscardPolicy();
-        ((ThreadPoolExecutor) service).setRejectedExecutionHandler(rejectedExecutionHandler);
-
-        service.shutdown();
-        service.submit(r); // silently ignore
-        service.awaitTermination(10, TimeUnit.SECONDS);
+        submitAfterShutdown(rejectedExecutionHandler); // silently ignore
     }
 
-    @Test(invocationCount = 5)
+    @Test
     public void saturationPolicyCallerRuns() throws InterruptedException {
-        int corePoolSize = 4;
-        int maximumPoolSize = 4;
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+        submitAfterShutdown(rejectedExecutionHandler); // silently ignore (!)
+    }
+
+    private void submitAfterShutdown(RejectedExecutionHandler rejectedExecutionHandler) throws InterruptedException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        ((ThreadPoolExecutor) executor).setRejectedExecutionHandler(rejectedExecutionHandler);
+
+        Runnable r = () -> log.info("running");
+
+        executor.shutdown();
+        executor.submit(r);
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
+
+    @Test(expectedExceptions = RejectedExecutionException.class)
+    public void saturationPolicyDefaultAbortQueueFull() throws InterruptedException {
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.AbortPolicy();
+        submitQueueFull(rejectedExecutionHandler); // throws RejectedExecutionException
+    }
+
+    @Test
+    public void saturationPolicyDiscardQueueFull() throws InterruptedException {
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.DiscardPolicy();
+        submitQueueFull(rejectedExecutionHandler); // silently ignore
+    }
+
+    private void submitQueueFull(RejectedExecutionHandler rejectedExecutionHandler) throws InterruptedException {
+        int corePoolSize = 2;
+        int maximumPoolSize = 2;
         long keepAliveTime = 1;
         TimeUnit unit = TimeUnit.MINUTES;
-        BlockingQueue<Runnable> workQueue = new ArrayBlockingQueue<>(50); // the smaller the bounded queue, the more likely for caller to run instead
-        ExecutorService service = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
-        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
-        ((ThreadPoolExecutor) service).setRejectedExecutionHandler(rejectedExecutionHandler);
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(5);
 
-        AtomicInteger counter = new AtomicInteger(0);
+        ExecutorService executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        ((ThreadPoolExecutor) executor).setRejectedExecutionHandler(rejectedExecutionHandler);
+
+        Runnable r = () -> log.info("running");
+
+        for (int i = 0; i < 100; i++) {
+            executor.submit(r);
+        }
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void saturationPolicyCallerRunsQueueFull() throws InterruptedException {
+        int corePoolSize = 2;
+        int maximumPoolSize = 2;
+        long keepAliveTime = 1;
+        TimeUnit unit = TimeUnit.MINUTES;
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(5); // the smaller the bounded queue, the more likely for caller to run instead
+
+        ExecutorService executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+
+        ((ThreadPoolExecutor) executor).setRejectedExecutionHandler(rejectedExecutionHandler);
+
+        AtomicInteger counter = new AtomicInteger();
         Runnable r = () -> {
             if (Thread.currentThread().getName().equals("main")) {
                 counter.incrementAndGet();
             }
         };
 
-        int threads = 1_000;
-        for (int i = 0; i < threads; i++) {
-            service.execute(r);
+        for (int i = 0; i < 100; i++) {
+            executor.submit(r);
         }
-        service.shutdown();
-        service.awaitTermination(10, TimeUnit.SECONDS);
-        log.info("Caller ran: {}", counter.get());
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+
+        assertThat(counter.get(), greaterThan(0));
     }
 }
