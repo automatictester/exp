88731792b3f58adf6f96ddb5f14153cb4fcfb802
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-05-30 21:26:04 +0200

    Use key generator for HmacSHA256 example

--- java/security/src/test/java/uk/co/automatictester/security/sign/HmacSha256.java
+++ java/security/src/test/java/uk/co/automatictester/security/sign/HmacSha256.java
@@ -3,8 +3,9 @@ package uk.co.automatictester.security.sign;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
+import javax.crypto.KeyGenerator;
 import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.SecretKey;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -12,15 +13,26 @@ import java.util.Base64;
 @Slf4j
 public class HmacSha256 {
 
+    private static final String ALGORITHM = "HmacSHA256";
+    private static final SecretKey KEY;
+
+    static {
+        KeyGenerator keyGenerator;
+        try {
+            keyGenerator = KeyGenerator.getInstance(ALGORITHM);
+            keyGenerator.init(256);
+            KEY = keyGenerator.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Test
     public void testHmacSha256() throws NoSuchAlgorithmException, InvalidKeyException {
         byte[] message = "Lorem ipsum dolor sit amet".getBytes();
-        byte[] key = "secret123key".getBytes();
-        String algorithm = "HmacSHA256";
 
-        Mac mac = Mac.getInstance(algorithm);
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key, algorithm);
-        mac.init(secretKeySpec);
+        Mac mac = Mac.getInstance(ALGORITHM);
+        mac.init(KEY);
         byte[] hmacSha256 = mac.doFinal(message);
 
         String base64encodedHmacSha265 = Base64.getEncoder().encodeToString(hmacSha256);
