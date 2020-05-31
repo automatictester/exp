a5a4d7e332818d1aa868b85fc66616b87f2bcc17
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-05-31 15:28:49 +0200

    Add Diffie-Hellman

---
+++ java/security/src/test/java/uk/co/automatictester/security/keyexchange/DiffieHellman.java
@@ -0,0 +1,49 @@
+package uk.co.automatictester.security.keyexchange;
+
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.security.SecureRandom;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Slf4j
+public class DiffieHellman {
+
+    private Party alice;
+    private Party bob;
+
+    @BeforeClass
+    public void setup() throws Exception {
+        alice = new Party("Alice");
+        String alicePublicKey = alice.getPublicKey();
+
+        bob = new Party("Bob", alicePublicKey);
+        String bobPublicKey = bob.getPublicKey();
+
+        alice.generateSecretKey(bobPublicKey);
+    }
+
+    @Test
+    public void testKeyExchange() throws Exception {
+        send(alice, bob, "message #1", getIv());
+        send(bob, alice, "message #2", getIv());
+        send(alice, bob, "message #3", getIv());
+        send(bob, alice, "message #4", getIv());
+    }
+
+    private void send(Party sender, Party recipient, String message, byte[] iv) throws Exception {
+        byte[] encrypted = sender.encrypt(message, iv);
+        String decrypted = recipient.decrypt(encrypted, iv);
+        assertThat(message, equalTo(decrypted));
+    }
+
+    private byte[] getIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        log.debug("Initialization vector: {}", iv);
+        return iv;
+    }
+}
---
+++ java/security/src/test/java/uk/co/automatictester/security/keyexchange/Party.java
@@ -0,0 +1,110 @@
+package uk.co.automatictester.security.keyexchange;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyAgreement;
+import javax.crypto.interfaces.DHPublicKey;
+import javax.crypto.spec.DHParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+@Slf4j
+public class Party {
+
+    private final KeyPair keyPair;
+    private final String name;
+    private final KeyAgreement keyAgreement;
+    private SecretKeySpec secretKey;
+
+    public Party(String name) throws Exception {
+        this.name = name;
+        keyPair = getKeyPair();
+        keyAgreement = KeyAgreement.getInstance("DH");
+        keyAgreement.init(keyPair.getPrivate());
+    }
+
+    public Party(String name, String otherPartyBase64EncodedPublicKey) throws Exception {
+        this.name = name;
+        PublicKey otherPartyPublicKey = base64ToPublicKey(otherPartyBase64EncodedPublicKey);
+        DHParameterSpec dhParamFromOtherPartyPublicKey = ((DHPublicKey) otherPartyPublicKey).getParams();
+        keyPair = getKeyPair(dhParamFromOtherPartyPublicKey);
+        keyAgreement = KeyAgreement.getInstance("DH");
+        keyAgreement.init(keyPair.getPrivate());
+        generateSecretKey(otherPartyPublicKey);
+    }
+
+    public void generateSecretKey(String otherPartyBase64EncodedPublicKey) throws Exception {
+        PublicKey otherPartyPublicKey = base64ToPublicKey(otherPartyBase64EncodedPublicKey);
+        generateSecretKey(otherPartyPublicKey);
+    }
+
+    private void generateSecretKey(PublicKey otherPartyPublicKey) throws Exception {
+        keyAgreement.doPhase(otherPartyPublicKey, true);
+        byte[] sharedSecret = keyAgreement.generateSecret();
+        secretKey = getSecretKey(sharedSecret);
+        log.info("{} secret key: {}", name, secretKey.getEncoded());
+    }
+
+    private KeyPair getKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = getKeyPairGenerator();
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    private KeyPair getKeyPair(DHParameterSpec dhParameterSpec) throws Exception {
+        KeyPairGenerator keyPairGenerator = getKeyPairGenerator();
+        keyPairGenerator.initialize(dhParameterSpec);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    private String publicKeyToBase64() {
+        return toBase64(keyPair.getPublic().getEncoded());
+    }
+
+    private String toBase64(byte[] data) {
+        return Base64.getEncoder().encodeToString(data);
+    }
+
+    private PublicKey base64ToPublicKey(String base64EncodedKey) throws Exception {
+        byte[] publicKey = Base64.getDecoder().decode(base64EncodedKey);
+        KeyFactory keyFactory = KeyFactory.getInstance("DH");
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(publicKey);
+        return keyFactory.generatePublic(x509KeySpec);
+    }
+
+    private SecretKeySpec getSecretKey(byte[] sharedSecret) {
+        return new SecretKeySpec(sharedSecret, 0, 16, "AES");
+    }
+
+    private KeyPairGenerator getKeyPairGenerator() throws Exception {
+        return KeyPairGenerator.getInstance("DH");
+    }
+
+    private Cipher getCipher() throws Exception {
+        return Cipher.getInstance("AES/CBC/PKCS5Padding");
+    }
+
+    public byte[] encrypt(String input, byte[] iv) throws Exception {
+        log.info("{} sent: '{}'", name, input);
+        Cipher cipher = getCipher();
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec(iv));
+        return cipher.doFinal(input.getBytes());
+    }
+
+    public String decrypt(byte[] encrypted, byte[] iv) throws Exception {
+        log.info("{} received: '{}'", name, encrypted);
+        Cipher cipher = getCipher();
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+        String decprypted = new String(cipher.doFinal(encrypted));
+        log.info("{} decrypted: '{}'", name, decprypted);
+        return decprypted;
+    }
+
+    public String getPublicKey() {
+        return publicKeyToBase64();
+    }
+}
