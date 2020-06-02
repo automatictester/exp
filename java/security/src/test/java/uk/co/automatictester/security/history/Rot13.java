35956d6d900f967aecb866b6b56c20cfffdf7b63
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-06-02 21:00:54 +0200

    Add Caesar and Rot13

---
+++ java/security/src/test/java/uk/co/automatictester/security/history/Rot13.java
@@ -0,0 +1,61 @@
+package uk.co.automatictester.security.history;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class Rot13 {
+
+    private static final int SHIFT = 13;
+    private static final int LOWER_BOUND = 65;
+    private static final int UPPER_BOUND = 90;
+
+    @Test
+    public void testEncrypt() {
+        String input = "ABCDEFXYZ";
+        String output = encrypt(input);
+        String expected = "NOPQRSKLM";
+        assertThat(expected, equalTo(output));
+
+        String reversed = encrypt(output);
+        assertThat(reversed, equalTo(input));
+    }
+
+    @DataProvider(name = "getInvalidData")
+    private Object[][] getInvalidData() {
+        return new Object[][]{
+                {"@"},
+                {"["}
+        };
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, dataProvider = "getInvalidData")
+    public void testInvalidInput(String input) {
+        encrypt(input);
+    }
+
+    private String encrypt(String s) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            validate(c);
+            char next = encryptChar(c);
+            if (next > UPPER_BOUND) {
+                int offset = next - UPPER_BOUND;
+                next = (char) (LOWER_BOUND + offset - 1);
+            }
+            output.append(next);
+        }
+        return output.toString();
+    }
+
+    private char encryptChar(char c) {
+        return (char) (c + SHIFT);
+    }
+
+    private void validate(char c) {
+        if (c < LOWER_BOUND || c > UPPER_BOUND) throw new IllegalArgumentException();
+    }
+}
